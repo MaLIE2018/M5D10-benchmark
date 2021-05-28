2ab097b114110feb9af23dcbd5990923824c1e11
MaLIE2018 <liebsch@pm.me>
2021-05-28 09:58:27 +0200

    initialization

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from "express"
+import cors from "cors"
+import createError from "http-errors"
+import { badRequest, catchAll, forbidden, notFound } from "./src/util/errorHandler.js"
+
+
+const app = express()
+
+const {port} =  process.env
+const whiteList = [process.env.WHITELIST_DEV_URL, process.env.WHITELIST_PROD_URL]
+
+const corsOptions = {
+  origin: function(origin, next){
+    if(whiteList.indexOf(origin) !== -1){
+      next(null,true)
+    }else{
+      next(createError(403, {"message":"Origin not allowed"}))
+    }
+  }
+}
+
+app.use(cors(corsOptions))
+app.use(express.json())
+
+
+
+
+
+app.use(badRequest, forbidden, notFound, catchAll)
+
+app.listen(port, () => {
+  console.log("Listening on port " + port)
+})
---
+++ src/util/errorHandler.js
@@ -0,0 +1,30 @@
+export const badRequest = (err, req, res, next) => {
+  if(err.status === 400){
+    res.status(400).send(err.message)
+  }else{
+    next(err)
+  }
+}
+export const forbidden = (err, req, res, next) => {
+  if(err.status === 403){
+    res.status(403).send(err.message)
+  }else{
+    next(err)
+  }
+}
+export const notFound = (err, req, res, next) => {
+  if(err.status === 404){
+    res.status(404).send(err.message)
+  }else{
+    next(err)
+  }
+}
+export const catchAll = (err, req, res, next) => {
+  if(err) res.status(500).send(err.message)
+  next()
+}
+
+
+
+
+
